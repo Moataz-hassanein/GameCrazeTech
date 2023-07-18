88ae9529de246588fdd13f3ee0ca444470d6a4ed
Moataz Hassanein <moataz.hassanein87@gmail.com>
2023-07-18 23:32:53 +0200

    the endpoint

--- client/src/apiCalls/productsApiCalls.js
+++ client/src/apiCalls/productsApiCalls.js
@@ -3,7 +3,9 @@ import axios from "axios";
 const getAllProducts = async (dispatch) => {
   try {
     // const response = await axios.get("http://localhost:8000/products");
-    const response = await axios.get("http://localhost:8000/products_gct");
+    const response = await axios.get(
+      "https://gamecrazetech.onrender.com/products_gct"
+    );
 
     dispatch({ type: "FETCH_PRODUCTS_SUCCESS", payload: response.data.data });
   } catch (error) {
